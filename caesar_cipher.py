68bd488a06801d24ff0e5f6ff1d1e99ffcb5bc68
L-Huber <71383238+L-Huber@users.noreply.github.com>
2023-10-15 15:03:14 +0200

    basic implementation
    -first version of implementation
    -with comments

---
+++ caesar_cipher.py
@@ -0,0 +1,28 @@
+#Gibt einen verschlüsselten Text aus, sowie eine Schlüssel zum Entschlüsseln des Textes
+
+    #Variabel:
+    #    input_string (str): Text, der verschlüsselt werden soll
+    #    shift_key (int): Anzahl an Buchstaben, die der Text verschoben werden soll
+
+    #Output:
+    #    Verschlüsselter Text als String
+    #    Zahl, um den Text wieder zu entschlüsseln
+def main():
+    input_string = "Dieser Satz wird nun verschluesselt"
+    shift_key =  3
+    caesar_cipher(input_string,shift_key)
+def caesar_cipher(input_string,shift_key):
+    print("Verschlüsselter Input:")
+    print(shifting(input_string,shift_key))
+    print("Zum Entschlüsseln folgenden shift_key verwenden:")
+    print(shift_key*(-1))
+
+def shifting(input_string,shift_key):
+    shifted_string=""
+    for char in input_string:
+        char_shifted = chr(ord(char) + shift_key)
+        shifted_string = shifted_string+char_shifted
+    return shifted_string
+
+if __name__ == "__main__":
+    main()
